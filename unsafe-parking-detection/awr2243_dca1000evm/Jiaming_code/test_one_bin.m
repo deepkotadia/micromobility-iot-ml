5bd8d752d35217fa2725fd81faf637c5d1987910
Deep Kotadia <deep.kotadia@gmail.com>
2021-11-24 02:15:33 -0600

    feat: file to process bin files

---
+++ unsafe-parking-detection/awr2243_dca1000evm/Jiaming_code/test_one_bin.m
@@ -0,0 +1,6 @@
+binFile = '../OriginalData/adc_data_1.bin';
+logFile = '../OriginalData/adc_data_1_LogFile.txt';
+paramsFile = '../OriginalData/parameters.m';
+heatmapsOut = '../PostProcessData/heatmaps';
+
+raw_data_process(binFile, logFile, paramsFile, heatmapsOut)
