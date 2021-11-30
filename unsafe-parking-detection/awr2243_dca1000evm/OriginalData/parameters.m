fe23b4d71ecb7c9a084262ac60cfb2563035d7e9
Deep Kotadia <deep.kotadia@rutgers.edu>
2021-11-30 11:37:00 -0600

    fix: update fmcw parameter values

--- unsafe-parking-detection/awr2243_dca1000evm/OriginalData/parameters.m
+++ unsafe-parking-detection/awr2243_dca1000evm/OriginalData/parameters.m
@@ -7,9 +7,9 @@ nADCBits = 16;
 c = 3e8;
 Fc = 77e9;
 slope = 60.012e12;
-idleTime = 50e-6;
+idleTime = 100e-6;
 adcStartTime = 6e-6;
-rampEndTime = 50e-6;
+rampEndTime = 60e-6;
 Fs = 10000e3;
 nSamples = 256;
 % Frame property
