bbd5f0269a59ab8ef28b7f975fd6196b605d3ae4
Beatrice Lovely <beatriceelovely@gmail.com>
2021-12-01 17:20:52 -0600

    added writing results to file

--- sidewalk-vs-street/models.py
+++ sidewalk-vs-street/models.py
@@ -5,38 +5,61 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
 from preprocess_data import read_all_stream_files_in_dir
+from datetime import datetime
+import os
 
+
+if not os.path.isdir("sidewalk-vs-street/imu_classifier_results"):
+    os.mkdir("sidewalk-vs-street/imu_classifier_results")
 
 def model_cross_val_stats(X, y):
+    results = dict()
     lgr_clf = LogisticRegression()
     print('Logistic Regression Classifier CV:')
-    print(cross_val_score(lgr_clf, X, y, cv=5))
+    lgr_res = cross_val_score(lgr_clf, X, y, cv=5)
+    results['Logistic Regression Classifier CV:'] = lgr_res
+    print(lgr_res)
     print('.....')
 
     svc_clf = LinearSVC(random_state=0)
     print('LinearSVC Classifier CV:')
-    print(cross_val_score(svc_clf, X, y, cv=5))
+    svc_res = cross_val_score(svc_clf, X, y, cv=5)
+    print(svc_res)
+    results['LinearSVC Classifier CV:'] = svc_res
     print('.....')
 
     knn_clf = KNeighborsClassifier()
     print('KNN Classifier CV:')
-    print(cross_val_score(knn_clf, X, y, cv=5))
+    knn_res =cross_val_score(knn_clf, X, y, cv=5)
+    print(knn_res)
+    results['KNN Classifier CV:'] = knn_res
     print('.....')
 
     gnb_clf = GaussianNB()
     print('Gaussian Naive Bayes Classifier CV:')
-    print(cross_val_score(gnb_clf, X, y, cv=5))
+    gnb_res = cross_val_score(gnb_clf, X, y, cv=5)
+    print(gnb_res)
+    results['Gaussian Naive Bayes Classifier CV:'] = gnb_res
     print('.....')
 
     gbc_clf = GradientBoostingClassifier()
     print('Gradient Boosting Classifier CV:')
-    print(cross_val_score(gbc_clf, X, y, cv=5))
+    gbc_res = cross_val_score(gbc_clf, X, y, cv=5)
+    print(gbc_res)
+    results['Gradient Boosting Classifier CV:'] = gbc_res
     print('.....')
 
     rfc_clf = RandomForestClassifier()
     print('Random Forest Classifier CV:')
-    print(cross_val_score(rfc_clf, X, y, cv=5))
+    rfc_res = cross_val_score(rfc_clf, X, y, cv=5)
+    print(rfc_res)
+    results['Random Forest Classifier CV:'] = rfc_res
     print('.....')
+    now = datetime.now()
+    date_time = now.strftime("%Y_%m_%d_%H_%M_%S")
+    print(date_time)
+    with open("sidewalk-vs-street/imu_classifier_results/street_classifier_{}.txt".format(date_time), mode='w') as res_file:
+        res_file.writelines(results)
 
 
 if __name__ == '__main__':
