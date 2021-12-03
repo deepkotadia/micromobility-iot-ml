e3b44f6fec7b154857303fdec2198e3bf617eb9e
Beatrice Lovely <beatriceelovely@gmail.com>
2021-12-03 12:58:24 -0600

    feat: return train score from cross val

--- sidewalk-vs-street/models.py
+++ sidewalk-vs-street/models.py
@@ -1,7 +1,7 @@
 from numpy.core.numeric import full
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, cross_validate
 from sklearn.linear_model import LogisticRegression
-from sklearn.svm import LinearSVC
+from sklearn.svm import LinearSVC, SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
@@ -9,72 +9,97 @@ from preprocess_data import read_all_stream_files_in_dir, shuffle_and_split
 from datetime import datetime
 import os
 
-WINDOW_SIZE = 150
+WINDOW_SIZE = 75
 
 if not os.path.isdir("sidewalk-vs-street/imu_classifier_results"):
     os.mkdir("sidewalk-vs-street/imu_classifier_results")
 
 def model_cross_val_stats(X, y):
+    now = datetime.now()
+    date_time = now.strftime("%Y_%m_%d_%H_%M_%S")
+    print(date_time)
+    res_file =  open("sidewalk-vs-street/imu_classifier_results/street_classifier_{}.txt".format(date_time), mode='w')
     results = dict()
     lgr_clf = LogisticRegression()
+    scores = dict()
     print('Logistic Regression Classifier CV:')
-    lgr_res = cross_val_score(lgr_clf, X, y, cv=5)
-    results['Logistic Regression Classifier CV:'] = lgr_res
+    lgr_res = cross_validate(lgr_clf, X, y, cv=5)
+    val_score = lgr_res['test_score'].mean()
+    train_scores = lgr_res['train_score']
+    res_file.write("Logistic Regression Classifier CV: val_score: {}, train_scores: {}".format(val_score, train_scores))
+    scores['val_score'] = val_score
+    scores['train_scores'] = train_scores
+    results['Logistic Regression Classifier CV:'] = scores
     print(lgr_res)
     print('.....')
 
-    svc_clf = LinearSVC(random_state=0)
-    print('LinearSVC Classifier CV:')
-    svc_res = cross_val_score(svc_clf, X, y, cv=5)
+    svc_clf = SVC(random_state=0, kernel='rbf')
+    print('SVC Classifier with rbf kernel CV:')
+    svc_res = cross_validate(svc_clf, X, y, cv=5)
+    val_score = svc_res['test_score'].mean()
+    train_scores = svc_res['train_score']
+    res_file.write("Logistic Regression Classifier CV: val_score: {}, train_scores: {}".format(val_score, train_scores) )
     print(svc_res)
-    results['LinearSVC Classifier CV:'] = svc_res
+    results['SVC Classifier with rbf kernel CV:'] = svc_res
     print('.....')
 
     knn_clf = KNeighborsClassifier()
     print('KNN Classifier CV:')
-    knn_res =cross_val_score(knn_clf, X, y, cv=5)
+    knn_res = cross_validate(knn_clf, X, y, cv=5)
+    val_score = knn_res['test_score'].mean()
+    train_scores = knn_res['train_score']
+    res_file.write("Logistic Regression Classifier CV: val_score: {}, train_scores: {}".format(val_score, train_scores) )
     print(knn_res)
     results['KNN Classifier CV:'] = knn_res
     print('.....')
 
     gnb_clf = GaussianNB()
     print('Gaussian Naive Bayes Classifier CV:')
-    gnb_res = cross_val_score(gnb_clf, X, y, cv=5)
+    gnb_res = cross_validate(gnb_clf, X, y, cv=5)
+    val_score = gnb_res['test_score'].mean()
+    train_scores = gnb_res['train_score']
+    res_file.write("Logistic Regression Classifier CV: val_score: {}, train_scores: {}".format(val_score, train_scores) )
     print(gnb_res)
     results['Gaussian Naive Bayes Classifier CV:'] = gnb_res
     print('.....')
 
-    gbc_clf = GradientBoostingClassifier()
+    gbc_clf = GradientBoostingClassifier(n_estimators=500)
     print('Gradient Boosting Classifier CV:')
-    gbc_res = cross_val_score(gbc_clf, X, y, cv=5)
+    gbc_res = cross_validate(gbc_clf, X, y, cv=5)
+    val_score = gbc_res['test_score'].mean()
+    train_scores =  gbc_res['train_score']
+    res_file.write("Logistic Regression Classifier CV: val_score: {}, train_scores: {}".format(val_score, train_scores) )
     print(gbc_res)
     results['Gradient Boosting Classifier CV:'] = gbc_res
     print('.....')
 
-    rfc_clf = RandomForestClassifier()
+    rfc_clf = RandomForestClassifier(n_estimators=500)
     print('Random Forest Classifier CV:')
-    rfc_res = cross_val_score(rfc_clf, X, y, cv=5)
+    rfc_res = cross_validate(rfc_clf, X, y, cv=5)
+    val_score = rfc_res['test_score'].mean()
+    train_scores = rfc_res['train_score']
+    res_file.write("Logistic Regression Classifier CV: val_score: {}, train_scores: {}".format(val_score, train_scores) )
     print(rfc_res)
     results['Random Forest Classifier CV:'] = rfc_res
     print('.....')
-    now = datetime.now()
-    date_time = now.strftime("%Y_%m_%d_%H_%M_%S")
-    print(date_time)
-    with open("sidewalk-vs-street/imu_classifier_results/street_classifier_{}.txt".format(date_time), mode='w') as res_file:
-        for key, value in results.items():
-            res_file.write(key +  "--->" + str(value) + "\n")
+
+    res_file.close()
+    
+    #with open("sidewalk-vs-street/imu_classifier_results/street_classifier_{}.txt".format(date_time), mode='w') as res_file:
+    #    for key, value in results.items():
+    #        res_file.write(key +  "--->" + str(value) + "\n")
 
 
 if __name__ == '__main__':
     full_quantized_df = read_all_stream_files_in_dir('IMU_Streams', window_size=WINDOW_SIZE)
 
-    shuffled_train_data = full_quantized_df.sample(frac=0.85)
-    
-    test_data = full_quantized_df.drop(shuffled_train_data.index).reset_index(drop=True)
-    print("number of training/val samples: ", shuffled_train_data.shape[0])
-    print("number of test samples: ", test_data.shape[0])
-    X = shuffled_train_data.reset_index(drop=True).iloc[:, :-1]
-    y = shuffled_train_data.iloc[:, -1]
+    #shuffled_train_data = full_quantized_df.sample(frac=0.85)
+    train, test = shuffle_and_split(full_quantized_df, test_size=0.001)
+    #test_data = full_quantized_df.drop(shuffled_train_data.index).reset_index(drop=True)
+    print("number of training/val samples: ", train.shape[0])
+    print("number of test samples: ", test.shape[0])
+    X = train.iloc[:, :-1]
+    y = train.iloc[:, -1]
 
     model_cross_val_stats(X, y)
 
